4935b383412d0ecb0448a87ba8218624295c06dc
ShadowCoder151 <shadowcoder023@gmail.com>
2024-12-28 10:13:04 +0530

    updated food_detect script

--- scripts/food_prediction.py
+++ scripts/food_prediction.py
@@ -13,4 +13,30 @@ def predict_label(image_path, model_path='models/food_model/model_training_1.pt'
     
     image = Image.open(image_path).convert("RGB")
     image = image.convert("L")  
+    image = image.resize(target_size)
     
+    input_tensor = transform(image)
+    input_tensor = input_tensor.unsqueeze(0)
+    input_tensor = input_tensor.to(device)
+    
+    with torch.no_grad():
+        output = model(input_tensor)
+        probabilities = torch.nn.functional.softmax(output, dim=1)
+        top_probs, top_classes = probabilities.topk(top_k, dim=1)
+
+    top_probs = top_probs.squeeze(0).cpu().numpy()
+    top_classes = top_classes.squeeze(0).cpu().numpy()
+    
+    label = top_classes[0]
+    return classes[label]
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+classes = {0: 'burger', 1: 'butter_naan', 2: 'chai', 3: 'chapati', 4: 'chole_bhature',
+           5: 'dal_makhani', 6: 'dhokla', 7: 'fried_rice', 8: 'idli', 9: 'jalebi',
+           10: 'kaathi_rolls', 11: 'kadai_paneer', 12: 'kulfi', 13: 'masala_dosa',
+           14: 'momos', 15: 'paani_puri', 16: 'pakode', 17: 'pav_bhaji',
+           18: 'pizza', 19: 'samosa'}
+
+image_path = "C:/Users/Welcome/Downloads/Dal-Makhani-Blog.jpg"
+label = predict_label(image_path, classes=classes, device=device)
+print(label)
